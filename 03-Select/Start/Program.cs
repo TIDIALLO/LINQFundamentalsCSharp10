09a8cbea56f41a7236ad3eaee5d6c253f59f2dfc
Tidiane Diallo <diallotidiane014@gmail.com>
2023-09-25 11:24:30 +0000

    Use Linq to Select Data whitin Collections

--- 03-Select/Start/Program.cs
+++ 03-Select/Start/Program.cs
@@ -4,7 +4,9 @@
 SamplesViewModel vm = new();
 
 // Call Sample Method
-var result = vm.GetAllQuery();
-
+//var result = vm.GetAllQuery();
+//var result = vm.GetSingleColumnQuery();
+//var result = vm.GetSingleColumnMethod();
+var result = vm.AnonymousClassQuery();
 // Display Results
 vm.Display(result);
